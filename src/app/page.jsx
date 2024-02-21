555f4fbb6e25c56de371edec2aa2c16a9f831188
eruotolo <edgardoruotolo@outlook.com>
2024-02-21 20:59:32 -0300

    Tarea: patrimonio home Fecha: 21-02-2024 Versión: 1.9

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import CarouselHome from '@/components/Carousel';
 import YoutubeVideo from '@/components/Youtube';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -15,6 +17,30 @@ export default function Home() {
             <section className="md:py-[100px]">
                 <div className="container mx-auto flex justify-center items-center align-middle">
                     <YoutubeVideo />
+                </div>
+            </section>
+            <section className="md:h-[900px] bg-beige md:py-[100px]">
+                <div className="container mx-auto flex">
+                    <div className="md:w-5/12">
+                        <Image
+                            src="/home-01.webp"
+                            alt="PATRIMONIO DE LA HUMANIDAD"
+                            width={1300}
+                            height={1503}
+                        />
+                    </div>
+                    <div className="md:w-7/12 md:pt-[140px] md:leading-[57px] ">
+                        <h1 className="text-tinto md:text-[40px] font-semibold md:mb-[20px]">
+                            PATRIMONIO DE LA HUMANIDAD
+                        </h1>
+                        <p className="text-tinto md:text-[26px] font-thin md:leading-[34px]">
+                            16 iglesias del archipiélago conforman el Sitio de Patrimonio Mundial,
+                            declarado por la UNESCO
+                        </p>
+                        <Link href="/" alt="PATRIMONIO DE LA HUMANIDAD">
+                            <Image src="/btn-mas.svg" alt="ir" width={240} height={60} />
+                        </Link>
+                    </div>
                 </div>
             </section>
         </main>
