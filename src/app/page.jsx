50570a4a51c454b95977fd273c56f8e7bcad3484
eruotolo <edgardoruotolo@outlook.com>
2024-02-22 19:43:59 -0300

    Tarea: Mapa Iglesias Primera Parte Fecha: 22-02-2024 Versión: 2.2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -36,7 +36,7 @@ export default function Home() {
                         </h1>
                         <p className="text-tinto md:text-[26px] font-thin md:leading-[34px]">
                             16 iglesias del archipiélago conforman el Sitio de Patrimonio Mundial,
-                            declarado por la UNESCO
+                            declarado por la UNESCO.
                         </p>
                         <Link href="/patrimonio" alt="PATRIMONIO DE LA HUMANIDAD">
                             <Image
@@ -44,14 +44,139 @@ export default function Home() {
                                 alt="ir"
                                 width={240}
                                 height={60}
-                                className="md:mt-[60px] md:w-[180px]"
+                                className="md:mt-[60px] md:w-[180px] hover:scale-[1.1]"
                             />
                         </Link>
                     </div>
                 </div>
             </section>
             <section className="md:py-[100px]">
-                <HotSpotsMapa />
+                <div className="container mx-auto">
+                    <HotSpotsMapa />
+                </div>
+            </section>
+            <section className="md:h-[900px] bg-beige">
+                <div className="container mx-auto flex">
+                    <div className="md:w-7/12">
+                        <h1 className="text-tinto md:text-[40px] font-semibold md:mb-[20px] md:mt-[-120px]">
+                            La Historia
+                        </h1>
+                        <p className="text-tinto md:text-[26px] font-thin md:leading-[34px]">
+                            Las iglesias de Chiloé son el resultado de un sincretismo cultural
+                            único, que se produce del rico intercambio entre españoles y pueblos
+                            originarios.
+                        </p>
+                        <Link href="/historia" alt="PATRIMONIO DE LA HUMANIDAD">
+                            <Image
+                                src="/btn-mas.svg"
+                                alt="ir"
+                                width={240}
+                                height={60}
+                                className="md:mt-[60px] md:w-[180px] hover:scale-[1.1]"
+                            />
+                        </Link>
+                    </div>
+                    <div className="md:w-5/12">
+                        <Image
+                            src="/home-02.webp"
+                            alt="La Historia"
+                            width={1300}
+                            height={1503}
+                            className="md:mt-[-380px]"
+                        />
+                    </div>
+                </div>
+            </section>
+            <section
+                className="bg-cover bg-no-repeat bg-center md:h-screen"
+                style={{ backgroundImage: "url('./home-03.webp')" }}
+            >
+                <div className="container mx-auto">
+                    <h1 className="font-barlow uppercase text-tinto md:text-[40px] md:font-semibold md:leading-[57px] md:pt-[270px] md:pl-[300px]">
+                        TESTIGOS DEL PASADO
+                    </h1>
+                </div>
+            </section>
+            <section className="md:h-[900px] md:py-[100px]">
+                <div className="container mx-auto flex">
+                    <div className="md:w-5/12">
+                        <Image src="/home-04.webp" alt="La Historia" width={1300} height={1462} />
+                    </div>
+                    <div className="md:w-7/12 md:pt-[170px] md:leading-[57px]">
+                        <h1 className="text-tinto md:text-[40px] font-semibold md:mb-[20px]">
+                            LA ARQUITECTURA
+                        </h1>
+                        <p className="text-tinto md:text-[26px] font-thin md:leading-[34px]">
+                            Las iglesias de Chiloé son la expresión monumental de la maestría y
+                            creatividad de la carpintería chilota.
+                        </p>
+                        <Link href="/arquitectura" alt="LA ARQUITECTURA">
+                            <Image
+                                src="/btn-mas.svg"
+                                alt="ir"
+                                width={240}
+                                height={60}
+                                className="md:mt-[60px] md:w-[180px] hover:scale-[1.1]"
+                            />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+            <section className="md:h-[900px] bg-beige md:py-[100px]">
+                <div className="container mx-auto flex">
+                    <div className="md:w-7/12 md:pt-[170px] md:leading-[57px]">
+                        <h1 className="text-tinto md:text-[40px] font-semibold md:mb-[20px]">
+                            LAS COMUNIDADES
+                        </h1>
+                        <p className="text-tinto md:text-[26px] font-thin md:leading-[34px]">
+                            Son las propias comunidades las que mantienen vivas a las iglesias y
+                            están a cargo su cuidado y mantención.
+                        </p>
+                        <Link href="/comunidades" alt="LAS COMUNIDADES">
+                            <Image
+                                src="/btn-mas.svg"
+                                alt="ir"
+                                width={240}
+                                height={60}
+                                className="md:mt-[60px] md:w-[180px] hover:scale-[1.1]"
+                            />
+                        </Link>
+                    </div>
+                    <div className="md:w-5/12">
+                        <Image
+                            src="/home-05.webp"
+                            alt="La Historia"
+                            width={1300}
+                            height={1895}
+                            className="md:mt-[-70px] md:h-[850px] md:w-auto"
+                        />
+                    </div>
+                </div>
+            </section>
+            <section
+                className="bg-cover bg-no-repeat bg-center md:h-screen"
+                style={{ backgroundImage: "url('./home-06.webp')" }}
+            >
+                <div className="container mx-auto flex justify-end items-center md:h-screen">
+                    <div className="md:w-6/12 md:leading-[57px]">
+                        <h1 className="text-blanco md:text-[40px] font-semibold md:mb-[20px]">
+                            LA ARQUITECTURA
+                        </h1>
+                        <p className="text-blanco md:text-[26px] font-thin md:leading-[34px]">
+                            Las iglesias de Chiloé son la expresión monumental de la maestría y
+                            creatividad de la carpintería chilota.
+                        </p>
+                        <Link href="/arquitectura" alt="LA ARQUITECTURA">
+                            <Image
+                                src="/btn-mas-wh.svg"
+                                alt="ir"
+                                width={240}
+                                height={60}
+                                className="md:mt-[60px] md:w-[180px] hover:scale-[1.1]"
+                            />
+                        </Link>
+                    </div>
+                </div>
             </section>
         </main>
     );
--- src/components/HotSpotsMapa.jsx
+++ src/components/HotSpotsMapa.jsx
@@ -6,10 +6,24 @@ import Link from 'next/link';
 import Mapa from '/public/mapa/mapa-sin-iglesias.webp';
 import Villipulli from '/public/mapa/villipulli.png';
 import Chonchi from '/public/mapa/chonchi.png';
+import Ichuac from '/public/mapa/ichuac.png';
+import Rilan from '/public/mapa/rilan.png';
+import Nercon from '/public/mapa/nercon.png';
+import Castro from '/public/mapa/castro.png';
+import Dalcahue from '/public/mapa/dalcahue.png';
+import SanJuan from '/public/mapa/sanjuan.png';
+import Colo from '/public/mapa/colo.png';
+import Tenaun from '/public/mapa/tenaun.png';
+import Achao from '/public/mapa/achao.png';
+import Caguach from '/public/mapa/caguach.png';
+import Quinchao from '/public/mapa/quinchao.png';
+import Chelin from '/public/mapa/chelin.png';
+import Detif from '/public/mapa/detif.png';
+import Aldachildo from '/public/mapa/aldachildo.png';
 
 export default function HotSpotsMapa() {
     return (
-        <section className="hotspots--wrapper">
+        <div className="hotspots--wrapper">
             <Image
                 src={Mapa}
                 alt="Mapa de las Iglesias"
@@ -21,28 +35,140 @@ export default function HotSpotsMapa() {
                 <Image
                     src={Villipulli}
                     alt="Iglesia de Villipulli"
-                    height={224}
                     width={249}
-                    className="img--villipulli"
+                    height={224}
+                    className="md:w-[110px] md:h-[122px]"
                 />
             </Link>
             <Link href="#" className="hotspot hotspot--Chonchi">
-                <Image src={Chonchi} alt="Iglesia de Chonchi" height={122} width={125} />
+                <Image src={Chonchi} alt="Iglesia de Chonchi" width={125} height={122} />
             </Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-            <Link href="#" className="hotspot"></Link>
-        </section>
+            <Link href="#" className="hotspot hotspot--Ichuac">
+                <Image
+                    src={Ichuac}
+                    alt="Iglesia de Ichuac"
+                    width={289}
+                    height={249}
+                    className="md:h-[122px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Rilan">
+                <Image
+                    src={Rilan}
+                    alt="Iglesia de Rilán"
+                    width={260}
+                    height={224}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Nercon">
+                <Image
+                    src={Nercon}
+                    alt="Iglesia de Nercón"
+                    width={289}
+                    height={249}
+                    className="md:h-[122px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Castro">
+                <Image
+                    src={Castro}
+                    alt="Iglesia de Castro"
+                    width={294}
+                    height={266}
+                    className="md:h-[122px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Dalcahue">
+                <Image
+                    src={Dalcahue}
+                    alt="Iglesia de Dalcahue"
+                    width={294}
+                    height={266}
+                    className="md:h-[126px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--SanJuan">
+                <Image
+                    src={SanJuan}
+                    alt="Iglesia de San Juan"
+                    width={294}
+                    height={266}
+                    className="md:h-[128px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Colo">
+                <Image
+                    src={Colo}
+                    alt="Iglesi de Colo"
+                    width={294}
+                    height={266}
+                    className="md:h-[128px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Tenaun">
+                <Image
+                    src={Tenaun}
+                    alt="Iglesia de Tenaún"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Achao">
+                <Image
+                    src={Achao}
+                    alt="Iglesia de Achao"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Caguach">
+                <Image
+                    src={Caguach}
+                    alt="Iglesia de Caguach"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Quinchao">
+                <Image
+                    src={Quinchao}
+                    alt="Iglesia de Quinchao"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Chelin">
+                <Image
+                    src={Chelin}
+                    alt="Iglesia de Chelin"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Detif">
+                <Image
+                    src={Detif}
+                    alt="Iglesia de Detif"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+            <Link href="#" className="hotspot hotspot--Aldachildo">
+                <Image
+                    src={Aldachildo}
+                    alt="Iglesia de Aldachildo"
+                    width={294}
+                    height={266}
+                    className="md:h-[130px] md:w-auto"
+                />
+            </Link>
+        </div>
     );
 }
