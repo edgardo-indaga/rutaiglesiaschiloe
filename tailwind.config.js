f7726116941a4fe7e64668bf65a261d9cdc764c0
eruotolo <edgardoruotolo@outlook.com>
2024-03-01 16:37:48 -0300

    Tarea: Update Galerias Fecha: 01-03-2024 Versión: 4.6

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
                 // => @media (min-width: 1024px) { ... }
                 xl: '1280px',
                 // => @media (min-width: 1280px) { ... }
-                '2xl': '1536px',
+                '2xl': '1280px',
                 // => @media (min-width: 1536px) { ... }
             },
         },
