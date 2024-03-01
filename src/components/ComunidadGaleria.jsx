b5d13b11fbb78d7b10d5b661e67f1bc2584aa07d
eruotolo <edgardoruotolo@outlook.com>
2024-03-01 12:26:50 -0300

    Tarea: Update Galerias Comunidades Fecha: 01-03-2024 Versión: 4.3

--- src/components/ComunidadGaleria.jsx
+++ src/components/ComunidadGaleria.jsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React from 'react';
+// Componente de Album Visual
 import PhotoAlbum from 'react-photo-album';
-
+// Componente de Lightbox (POP-UP) de las imagenes
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
-
+// Componente de Lightbox para poner titulos y descripciones
+import Captions from 'yet-another-react-lightbox/plugins/captions';
+import 'yet-another-react-lightbox/plugins/captions.css';
 // import optional lightbox plugins
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
 import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
@@ -14,14 +17,62 @@ import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
 const slides = [
-    { src: '/comunidades/gallery/comunidades_galeria_01.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_02.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_03.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_04.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_05.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_06.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_07.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_08.jpg', width: 1200, height: 800 },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_01.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Detif ',
+        description: '',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_02.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Caguach',
+        description: 'Fiesta patronal de Jesús Nazareno',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_03.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Aldachildo',
+        description: 'Iglesia adornada por celebración religiosa',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_04.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Chelín',
+        description: 'Preparación de celebración religiosa',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_05.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Quinchao',
+        description: 'Imagen adornada por fiesta religiosa',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_06.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Nercón',
+        description: 'Imagen de San Miguel y banderines utilizados en pasacalles',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_07.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Caguach',
+        description: 'Fiesta patronal de Jesús Nazareno',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_08.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Caguach',
+        description: 'Fiesta patronal de Jesús Nazareno',
+    },
 ];
 
 export default function ComunidadGallery() {
@@ -42,7 +93,7 @@ export default function ComunidadGallery() {
                 index={index}
                 close={() => setIndex(-1)}
                 // enable optional lightbox plugins
-                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom, Captions]}
             />
         </>
     );
--- src/components/ComunidadGaleriaSecundaria.jsx
+++ src/components/ComunidadGaleriaSecundaria.jsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React from 'react';
+// Componente de Album Visual
 import PhotoAlbum from 'react-photo-album';
-
+// Componente de Lightbox (POP-UP) de las imagenes
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
-
+// Componente de Lightbox para poner titulos y descripciones
+import Captions from 'yet-another-react-lightbox/plugins/captions';
+import 'yet-another-react-lightbox/plugins/captions.css';
 // import optional lightbox plugins
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
 import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
@@ -14,14 +17,62 @@ import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
 const slidesSecundario = [
-    { src: '/comunidades/gallery/comunidades_galeria_09.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_10.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_11.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_12.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_13.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_14.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_15.jpg', width: 1200, height: 800 },
-    { src: '/comunidades/gallery/comunidades_galeria_16.jpg', width: 1200, height: 800 },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_09.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Chonchi',
+        description: 'María Aguilar, Encargada de la iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_10.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Aldachildo',
+        description: 'Jorge Gómez, Fiscal de la iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_11.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Detif',
+        description: 'Bertila Nahuelquin, Encargada de la Iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_12.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Chelín',
+        description: 'Rudith Concha, Patrona de la Iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_13.jpg',
+        width: 1333,
+        height: 2000,
+        title: 'Iglesia de Ichuac',
+        description: 'Amanda Aro, Fiscala de la Iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_14.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Nercón',
+        description: 'Isabel Naguelquín, Patrona de la iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_15.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de San Juan',
+        description: 'Encargado de la iglesia',
+    },
+    {
+        src: '/comunidades/gallery/comunidades_galeria_16.jpg',
+        width: 1200,
+        height: 800,
+        title: 'Iglesia de Tenaún',
+        description: 'Juanita Ortiz, Encargada de la Iglesia',
+    },
 ];
 
 export default function ComunidadGallerySecundaria() {
@@ -42,7 +93,7 @@ export default function ComunidadGallerySecundaria() {
                 index={index}
                 close={() => setIndex(-1)}
                 // enable optional lightbox plugins
-                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom, Captions]}
             />
         </>
     );
