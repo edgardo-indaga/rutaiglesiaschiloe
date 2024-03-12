4d4be2c42f9f6f5ca6ed55b93b6469cab0641565
eruotolo <edgardoruotolo@outlook.com>
2024-03-12 14:00:58 -0300

    Tarea: Incorporar Favicon Fecha: 12-03-2024 Versión: 6.3

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 const barlow = Barlow({
     subsets: ['latin'],
@@ -55,6 +56,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
+            <Head>
+                <link rel="icon" href="/favicon.svg" sizes="32x32" type="image/svg+xml" />
+                <link rel="icon" href="/favicon.png" sizes="64x64" type="image/png" />
+            </Head>
             <body className={barlow.className}>
                 <Header />
                 {children}
