692f994ecc42ba7a449c8864afb3d9506c9f1308
eruotolo <edgardoruotolo@outlook.com>
2024-03-14 15:17:09 -0300

    Tarea: Vercel Analytics Fecha: 14-03-2024 Versión: 8.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Barlow } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -67,6 +68,7 @@ export default function RootLayout({ children }) {
                 <Header />
                 {children}
                 <Footer />
+                <Analytics />
             </body>
         </html>
     );
