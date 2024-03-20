1cfcc0bb46e50314be1fe50b4e2d87241ac818ec
eruotolo <edgardoruotolo@outlook.com>
2024-03-20 19:28:01 -0300

    Tarea: Mapa Fecha: 20-03-2024 Versión: 9.0

--- src/components/MapaCastro.jsx
+++ src/components/MapaCastro.jsx
@@ -10,14 +10,23 @@ const Map = () => {
     const mapContainer = useRef(null);
     const lng = -73.7638; // longitud del centro del mapa
     const lat = -42.4816; // latitud del centro del mapa
+    let zoom;
 
     useEffect(() => {
+        if (window.innerWidth <= 480) {
+            zoom = 14; // asignamos un valor de zoom para pantallas pequeñas
+        } else {
+            zoom = 16; // asignamos un valor de zoom para pantallas grandes
+        }
+
         const map = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/eruotolo/clu0c62dz004g01qq699vaglo',
             center: [lng, lat],
-            zoom: 17,
+            zoom: zoom,
         });
+
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
         new mapboxgl.Marker({ color: '#911443' }).setLngLat([lng, lat]).addTo(map);
 
@@ -25,8 +34,8 @@ const Map = () => {
     }, []);
 
     return (
-        <div className="container mx-auto my-[50px]">
-            <div ref={mapContainer} className="h-[560px]" />
+        <div className="container mx-auto my-[50px] md:px-20">
+            <div ref={mapContainer} className="h-[350px] md:h-[400px]" />
         </div>
     );
 };
