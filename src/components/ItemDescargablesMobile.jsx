0f95b8ac6bfb983a5e8f034afe459f28c1026260
eruotolo <edgardoruotolo@outlook.com>
2024-04-10 18:17:08 -0400

    Tarea: NUEVO PDF Fecha: 10-04-2024 Versión: 10.3

--- src/components/ItemDescargablesMobile.jsx
+++ src/components/ItemDescargablesMobile.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import thumbItem01 from '/public/descargables/thumb-item01.webp';
+import thumbItem02 from '/public/descargables/thumb-item02.webp';
 
 export default function ThumbDescargablesMobile() {
     return (
@@ -15,6 +16,21 @@ export default function ThumbDescargablesMobile() {
                         <Image
                             src={thumbItem01}
                             alt="Protocolo"
+                            quality={100}
+                            style={{
+                                objectFit: 'cover',
+                            }}
+                        />
+                    </div>
+                </Link>
+                <Link href="/uploads/pasaporte.pdf" className="my-5" target={'_blank'}>
+                    <div className="w-[320px] h-[366]">
+                        <h2 className="text-beige text-[18px] font-[600] absolute mt-[20px] ml-[20px]">
+                            PASAPORTE
+                        </h2>
+                        <Image
+                            src={thumbItem02}
+                            alt="Pasaporte"
                             quality={100}
                             style={{
                                 objectFit: 'cover',
