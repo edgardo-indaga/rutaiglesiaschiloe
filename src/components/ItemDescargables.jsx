bc6f0331b7c300a05715cffced7753f6dd97763d
eruotolo <edgardoruotolo@outlook.com>
2024-04-16 18:04:18 -0400

    Tarea: QR PDF Fecha: 16-04-2024 Versión: 10.9

--- src/components/ItemDescargables.jsx
+++ src/components/ItemDescargables.jsx
@@ -11,6 +11,7 @@ const style = {
 export default function ThumbDescargables() {
     const [isHovereditem01, setIsHovereditem01] = useState(false);
     const [isHovereditem02, setIsHovereditem02] = useState(false);
+    const [isHovereditem03, setIsHovereditem03] = useState(false);
 
     return (
         <section className="hidden py-[40px] px-[5px] md:py-[100px] md:px-0 xl:block">
@@ -44,6 +45,7 @@ export default function ThumbDescargables() {
                         </Link>
                     </div>
                 </div>
+
                 <div className="flex w-6/12 items-center justify-center align-middle mb-[15px] md:mb-[25px] md:w-3/12">
                     <div
                         onMouseEnter={() => setIsHovereditem02(true)}
@@ -70,6 +72,36 @@ export default function ThumbDescargables() {
                             }`}
                         >
                             PASAPORTE
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="flex w-6/12 items-center justify-center align-middle mb-[15px] md:mb-[25px] md:w-3/12">
+                    <div
+                        onMouseEnter={() => setIsHovereditem03(true)}
+                        onMouseLeave={() => setIsHovereditem03(false)}
+                    >
+                        <div
+                            className={`md:w-[300] md:h-[300px] ${isHovereditem03 ? 'hidden' : ''}`}
+                        >
+                            <Image
+                                src="/qr/fichageneral.webp"
+                                alt="Item 01"
+                                height={300}
+                                width={300}
+                                priority={true} // Prioritize the image for faster loading
+                                quality={100} // Maintain image quality
+                                className={style.imgResposive}
+                            />
+                        </div>
+                        <Link
+                            href="/uploads/fichageneral-es.pdf"
+                            target={'_blank'}
+                            className={`md:w-[300px] md:h-[300px] md:px-[20px] bg-beige flex justify-center items-center text-tinto md:text-[20px] md:font-medium uppercase text-center ${
+                                isHovereditem03 ? '' : 'hidden'
+                            }`}
+                        >
+                            FICHA GENERAL
                         </Link>
                     </div>
                 </div>
