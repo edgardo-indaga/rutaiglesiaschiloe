366f109ec98f03824920017b98dd7879fa58a8e4
eruotolo <edgardoruotolo@outlook.com>
2024-06-24 18:47:41 -0400

    Tarea: SiteMap y Robots Fecha: 24-06-2024 Versión: 12.4

--- src/app/robots.js
+++ src/app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://www.rutaiglesiaschiloe.cl/sitemap.xml',
+        sitemap: 'https://rutaiglesiaschiloe.cl/sitemap.xml',
     };
 }
