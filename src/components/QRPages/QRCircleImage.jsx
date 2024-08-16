f727aae6bece9a810f9c972d796e3e36f30dd4a7
eruotolo <edgardoruotolo@outlook.com>
2024-08-16 17:23:53 -0400

    Tarea: Update QR Fecha: 16-08-2024 Versión: 14.3

--- src/components/QRPages/QRCircleImage.jsx
+++ src/components/QRPages/QRCircleImage.jsx
@@ -9,7 +9,7 @@ export default function QRCircleImage({ imageCircle }) {
                     alt={'Imagen Circulo'}
                     width={470}
                     height={470}
-                    className="border-[6px] border-tinto rounded-[50%] w-[550px] h-[550px] object-cover"
+                    className="border-[6px] border-tinto rounded-[50%] w-[360px] h-[360px] md:w-[550px] md:h-[550px] object-cover"
                 />
             </div>
         </>
